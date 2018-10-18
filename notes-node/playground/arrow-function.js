54680a357e0ebf50d2a3fa927cdeaa29b892e3d3
Terence Yak <yakkiansheng@gmail.com>
2018-10-19 06:21:11 +0800

    Completed note app

---
+++ notes-node/playground/arrow-function.js
@@ -0,0 +1,18 @@
+var square = (x) => x * x;
+console.log(square(9));
+
+var user = {
+    name: 'Terence',
+    sayHi: () => {
+        console.log(arguments);
+        console.log(`Hi, I'm ${this.name}`);
+    },
+    sayHiAlt() {
+        console.log(arguments);
+        console.log(`Hi, I'm ${this.name}`);
+    }
+};
+
+user.sayHi();
+user.sayHiAlt(1,2,3);
+
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Terence'
+};
+
+person.age = 25;
+
+person.name = 'Terence';
+
+console.log(person);
